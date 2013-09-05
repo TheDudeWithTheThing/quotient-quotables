3d72707c48c8ede8ceb8ddad11784a6cd8d13723
Shaun Butler <shaun.butler@gmail.com>
2013-09-05 12:18:51 -0400

    finished

--- web.go
+++ web.go
@@ -4,10 +4,20 @@ import (
     "fmt"
     "net/http"
     "os"
+    "encoding/json"
+    "github.com/gorilla/mux"
+    "io/ioutil"
+    "strings"
+    "math/rand"
 )
 
 func main() {
-    http.HandleFunc("/", hello)
+    var router = mux.NewRouter()
+
+    router.HandleFunc("/quote/{author}", QuoteHandler)
+
+    http.Handle("/", router)
+
     fmt.Println("listening...")
     err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
     if err != nil {
@@ -15,6 +25,29 @@ func main() {
     }
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintln(res, "hello, world")
+func QuoteHandler(res http.ResponseWriter, req *http.Request) {
+  res.Header().Set("Content-type", "application/json; charset=utf-8")
+  // see if file exists
+  // read first line for full author name
+  // make json
+  vars := mux.Vars(req)
+  author := vars["author"]
+  authorFile := "./" + author + ".quote"
+  if _, err := os.Stat(authorFile); os.IsNotExist(err) {
+    // return 404
+  } else {
+    fileContents, _ := ioutil.ReadFile(authorFile)
+    splitFileContents := strings.Split(string(fileContents), "\n")
+    authorName := splitFileContents[0]
+    splitFileContents = splitFileContents[1:]
+    quoteIndex := rand.Int31n(int32(len(splitFileContents) - 1))
+    quote := splitFileContents[quoteIndex]
+
+    m := make(map[string]string)
+    m["author"] = authorName
+    m["quote"] = quote
+    value, _ := json.Marshal(m)
+    fmt.Fprintln(res, string(value))
+  }
+
 }
