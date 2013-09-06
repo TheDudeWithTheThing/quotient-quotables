f972f5c48926835af9c9273a6bff321902399fca
Shaun Butler <shaun.butler@gmail.com>
2013-09-06 15:32:23 -0400

    now uses redis as datastore instead of filesystem

--- web.go
+++ web.go
@@ -1,53 +1,112 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "os"
     "encoding/json"
+    "fmt"
+    "github.com/garyburd/redigo/redis"
     "github.com/gorilla/mux"
     "io/ioutil"
+    "net/http"
+    "net/url"
+    "os"
+    "path/filepath"
     "strings"
-    "math/rand"
 )
+
+var server, password string = ParseRedistogoUrl()
+
+var pool = &redis.Pool {
+  MaxIdle: 3,
+  IdleTimeout: 240,
+  MaxActive: 10,
+  Dial: func () (redis.Conn, error) {
+    c, err := redis.Dial("tcp", server)
+    if err != nil {
+      return nil, err
+    }
+    if _, err := c.Do("AUTH", password); err != nil && len(password) > 0 {
+      c.Close()
+      return nil, err
+    }
+    return c, err
+  },
+}
 
 func main() {
     var router = mux.NewRouter()
 
     router.HandleFunc("/quote/{author}", QuoteHandler)
+    router.HandleFunc("/load", LoadHandler)
 
     http.Handle("/", router)
 
     fmt.Println("listening...")
     err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
     if err != nil {
+      fmt.Println("panicing...")
       panic(err)
     }
 }
 
-func QuoteHandler(res http.ResponseWriter, req *http.Request) {
-  res.Header().Set("Content-type", "application/json; charset=utf-8")
-  // see if file exists
-  // read first line for full author name
-  // make json
-  vars := mux.Vars(req)
-  author := vars["author"]
-  authorFile := "./" + author + ".quote"
-  if _, err := os.Stat(authorFile); os.IsNotExist(err) {
-    // return 404
-  } else {
-    fileContents, _ := ioutil.ReadFile(authorFile)
+
+func LoadHandler(res http.ResponseWriter, req *http.Request) {
+  var client = pool.Get()
+  defer client.Close()
+  quoteFiles, _ := filepath.Glob("*.quote")
+
+  for _, file := range quoteFiles {
+    fileContents, _ := ioutil.ReadFile(file)
     splitFileContents := strings.Split(string(fileContents), "\n")
     authorName := splitFileContents[0]
     splitFileContents = splitFileContents[1:]
-    quoteIndex := rand.Int31n(int32(len(splitFileContents) - 1))
-    quote := splitFileContents[quoteIndex]
 
-    m := make(map[string]string)
-    m["author"] = authorName
-    m["quote"] = quote
-    value, _ := json.Marshal(m)
-    fmt.Fprintln(res, string(value))
+    uri := strings.Split(file, ".")
+    redisKey := "quote:" + uri[0]
+
+    client.Do("SET", "realname:" + uri[0], []byte(authorName))
+
+    for _, quote := range splitFileContents {
+      if len(quote) > 0  {
+        client.Do("SADD", redisKey, []byte(quote))
+      }
+    }
   }
 
+  fmt.Fprintln(res, "loading complete")
+}
+
+func QuoteHandler(res http.ResponseWriter, req *http.Request) {
+  var client = pool.Get()
+  defer client.Close()
+  vars := mux.Vars(req)
+  author := vars["author"]
+  authorName, err := redis.String(client.Do("GET", "realname:" + author))
+  if err != nil {
+    http.NotFound(res, req)
+  } else {
+    quote, _ := redis.String(client.Do("SRANDMEMBER", "quote:" + author))
+
+    m := make(map[string]string)
+    m["author"] = string(authorName)
+    m["quote"] = string(quote)
+    value, _ := json.Marshal(m)
+
+    res.Header().Set("Content-type", "application/json; charset=utf-8")
+    fmt.Fprintln(res, string(value))
+  }
+}
+
+func ParseRedistogoUrl() (string, string) {
+  redisUrl := os.Getenv("REDISTOGO_URL")
+  fmt.Println(redisUrl)
+
+  redisInfo, _ := url.Parse(redisUrl)
+  server := redisInfo.Host
+  fmt.Println("SERVER " + server)
+
+  password := ""
+  if redisInfo.User != nil {
+    password, _ = redisInfo.User.Password()
+  }
+  return server, password
 }
